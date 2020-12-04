53695b020dbb272fa544457092e896a2b6d70267
kaihara_hiroto <hirotok1435@icloud.com>
2020-12-04 20:33:23 +0900

    profile-edit function

--- asovi_app/views.py
+++ asovi_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ProfileForm
+from .models import Profile
+
 
 # Create your views here.
+
+def profile_edit(request):
+    obj = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        profile = ProfileForm(request.POST, instance=obj)
+        profile.save()
+    params = {
+        'form': ProfileForm(instance=obj),
+    }
+    return render(request,'asovi_app/profile_edit.html',params)
