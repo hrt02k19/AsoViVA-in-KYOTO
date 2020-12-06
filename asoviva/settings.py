e3f305d107c080b76bf0fdc21ade5e2672525e5e
fkk <kinako.fukiko.0822@gmail.com>
2020-12-06 11:35:56 +0900

    本登録後Profile編集画面に遷移

--- asoviva/settings.py
+++ asoviva/settings.py
@@ -183,6 +183,7 @@ ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_SIGNUP_REDIRECT_URL = = 'asovi_app:profile-edit'  # 本登録後Profile設定ページに遷移
 
 try:
     from .local_settings import *
