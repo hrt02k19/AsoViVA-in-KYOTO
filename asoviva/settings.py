165e4f66ca34760e696fa2282b683d811ef5795e
fkk <kinako.fukiko.0822@gmail.com>
2020-12-06 11:21:57 +0900

    allauthカスタムテンプレートディレクトリ設置

--- asoviva/settings.py
+++ asoviva/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -96,7 +97,9 @@ ROOT_URLCONF = 'asoviva.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'accounts', 'templates', 'account'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
