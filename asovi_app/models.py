48772f6dc4b7413fa896c6c270869f8892bff2b5
kaihara_hiroto <hirotok1435@icloud.com>
2021-02-02 00:28:18 +0900

    投稿機能の修正

--- asovi_app/models.py
+++ asovi_app/models.py
@@ -154,7 +154,7 @@ class NotificationSetting(models.Model):
 class Post(models.Model):
     posted_by=models.ForeignKey(CustomUser,related_name='posted_by',on_delete=SET_NULL,null=True)
     image=models.ImageField(upload_to="static/asovi_app/img")
-    genre=models.ManyToManyField(Genre,related_name='post_genre',blank=True)
+    genre=models.ForeignKey(Genre,related_name='post_genre',on_delete=SET_NULL,null=True,blank=True)
     time=models.DateTimeField(auto_now_add=True,null=True)
     body=models.TextField(max_length=300)
     latitude=models.FloatField(default=0)
